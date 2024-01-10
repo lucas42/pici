c60b8ac55b35bd96ac2307e8bb0a2f229cc6550a
Luke Blaney <git@lukeblaney.co.uk>
2024-01-10 22:00:27 +0000

    set git repo to correct branch (defaulting to main)

--- quickbuild.sh
+++ quickbuild.sh
@@ -5,6 +5,7 @@ directory=$(mktemp -d)
 cd $directory
 git clone $CIRCLE_REPOSITORY_URL
 cd "$(basename "$CIRCLE_REPOSITORY_URL" .git)"
+git checkout ${CIRCLE_BRANCH:=main}
 docker-compose build
 echo "$DOCKERHUB_ACCESS_TOKEN" | docker login --username "$DOCKERHUB_USERNAME" --password-stdin
 docker-compose push
